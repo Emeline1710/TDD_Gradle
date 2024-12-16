2ddbbd94d88ea6d7c331451d315a3a60fc15161f
Emeline <66369599+Emeline1710@users.noreply.github.com>
2024-12-16 10:41:41 +0100

    ajout de Jacoco

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,37 +1,41 @@
 plugins {
-    id("java")
+    java
     id("org.sonarqube") version "5.1.0.4882"
+    jacoco
 }
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
 
 repositories {
-    gradlePluginPortal()
-    google()
     mavenCentral()
-    maven {
-        url = uri("https://plugins.gradle.org/m2/")
-    }
+    gradlePluginPortal()
 }
 
 dependencies {
-    implementation("junit:junit:4.13.1")
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
-sonarqube  {
+sonarqube {
     properties {
         property("sonar.projectKey", "TDD")
         property("sonar.projectName", "TDD")
         property("sonar.sources", "src/main/java")
         property("sonar.host.url", "http://localhost:9000")
         property("sonar.token", "sqp_c8dd8750bb8e7a5f84633fbd653c294a7384c050")
-        property("sonar.java.binaries", file("build/classes"))
+        property("sonar.java.binaries", "build/classes/java/main")
     }
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required.set(true)
+        html.required.set(true)
+    }
 }
